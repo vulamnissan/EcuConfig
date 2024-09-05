dd8683625f98e60114063b4970646a44eb888685
VU LAM <knt12017@Lams-MacBook-Pro.local>
2024-09-05 18:47:04 +0700

    Upload

--- login_app/views.py
+++ login_app/views.py
@@ -6,7 +6,6 @@ from django.http import HttpResponse
 def view_login_page(request):
   template = loader.get_template('login/login_page.html')
   return HttpResponse(template.render())
-
 def view_forgot_pass(request):
   template = loader.get_template('login/forgot_pass.html')
   return HttpResponse(template.render())
