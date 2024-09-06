ac3a0477fadf3ea395d384241e7becf3e6cea350
dinhtrong5120 <102452134+dinhtrong5120@users.noreply.github.com>
2024-09-06 16:33:21 +0700

    sua url

--- login_app/urls.py
+++ login_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('u', views.view_login_page, name='login_view'),
+    path('', views.view_login_page, name='login_view'),
     path('forgot_pass/', views.view_forgot_pass, name='forgot_pass'),
 ]
